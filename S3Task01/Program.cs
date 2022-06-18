e92b086f3c0cc6f45c7d54743c076756c0f8ae05
Jeiojey <signofaries@gmail.com>
2022-06-18 20:10:38 +0300

    First commit

---
+++ S3Task01/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+using System;
+using static System.Console;
+Clear();
+
+WriteLine("Введите пятизначное число: ");
+int number = int.Parse(ReadLine());
+
+if  (number < 10000 || number > 99999)
+{
+WriteLine("Число не пятизначное! Попробуйте еще раз");
+}
+else
+{
+    string answer = number / 10000 == number % 10 & number / 1000 % 10 == number % 100 / 10 ? "да" : "нет";
+WriteLine(answer);
+}
---
+++ S3Task02/Program.cs
@@ -0,0 +1,26 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+
+using System;
+using static System.Console;
+Clear();
+
+WriteLine("Введите координаты А: ");
+int X1 = int.Parse(ReadLine());
+int Y1 = int.Parse(ReadLine());
+int Z1 = int.Parse(ReadLine());
+
+WriteLine("Введите координату B: ");
+int X2 = int.Parse(ReadLine());
+int Y2 = int.Parse(ReadLine());
+int Z2 = int.Parse(ReadLine());
+
+WriteLine($"А ({X1},{Y1},{Z1}); B ({X2},{Y2},{Y2})");
+
+int X = X1 - X2;
+int Y = Y1 - Y2;
+int Z = Z1 - Z2;
+
+double C = Math.Sqrt(Math.Pow(X, 2) + Math.Pow(Y, 2)+ Math.Pow(Z,2));
+WriteLine($"{C:f2}");
